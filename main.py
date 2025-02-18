ab097f08bd786f8fb82dd0a39ffd1481c81ed373
VadoVates <marekgor@gmail.com>
2025-02-18 07:07:58 +0100

    changed problematic name "timestamp" to "created_at" and shut down of ssl mode, because all is in a container.

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from pydantic import BaseModel
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, Column, DateTime, Float, Integer
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
-from starlette.responses import StreamingResponse
+from starlette.responses import StreamingResponse, Response
 
 app = FastAPI()
 
 load_dotenv()
 DATABASE_URL = (os.getenv("DB_PROTOCOL") + '://' + os.getenv("DB_USER") + ':' + os.getenv("DB_PASSWORD") + '@'
-                + os.getenv("DB_HOST") + ':' + os.getenv("DB_PORT") + '/' + os.getenv("DB_NAME") + '?sslmode=require')
+                + os.getenv("DB_HOST") + ':' + os.getenv("DB_PORT") + '/' + os.getenv("DB_NAME"))
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -27,7 +27,7 @@ class TemperatureReading(Base):
     id = Column(Integer, primary_key=True, index=True)
     temperature = Column(Float, nullable=False)
     humidity = Column(Float, nullable=False)
-    timestamp = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
 
 Base.metadata.create_all(bind=engine)
 
@@ -50,24 +50,24 @@ async def temperature(data: TemperatureData, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(reading)
 
-    timestamp = datetime.now().isoformat()
-    print(f"[{timestamp}] Received data: Temp={data.temperature}°C, Humidity={data.humidity}%")
+    created_at = datetime.now().isoformat()
+    print(f"[{created_at}] Received data: Temp={data.temperature}°C, Humidity={data.humidity}%")
     return {
         "status": "ok",
         "data": reading.temperature,
         "humidity": reading.humidity,
-        "timestamp": reading.timestamp,
+        "created_at": reading.created_at,
     }
 
 @app.get("/history")
 async def history(db: Session = Depends(get_db)):
-    readings = db.query(TemperatureReading).order_by(TemperatureReading.timestamp.desc()).limit(10).all()
+    readings = db.query(TemperatureReading).order_by(TemperatureReading.created_at.desc()).limit(10).all()
     return [
         {
             "id" : r.id,
             "temperature" : r.temperature,
             "humidity" : r.humidity,
-            "timestamp" : r.timestamp
+            "created_at" : r.created_at
         }
         for r in readings
     ]
@@ -78,14 +78,18 @@ async def stream(db: Session = Depends(get_db)):
         last_id = None
 
         while True:
-            latest = db.query(TemperatureReading).order_by(TemperatureReading.timestamp.desc()).first()
+            latest = db.query(TemperatureReading).order_by(TemperatureReading.created_at.desc()).first()
             if latest and latest.id != last_id:
                 last_id = latest.id
 
-                yield f"data: {latest.temperature},{latest.humidity},{latest.timestamp}\n\n"
+                yield f"data: {latest.temperature},{latest.humidity},{latest.created_at}\n\n"
 
             await asyncio.sleep(5)
     return StreamingResponse(event_generator(), media_type="text/event-stream")
+
+@app.head("/")
+async def head():
+    return Response(status_code=200)
 
 @app.get("/")
 async def root():
